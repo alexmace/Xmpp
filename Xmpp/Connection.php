04ce58711160a48aab91f77476d49a1b21759424
Alex Mace <alex@hollytree.co.uk>
2011-11-06 17:09:48 +0000

    Improved how encoding is done for sending messages

--- Xmpp/Connection.php
+++ Xmpp/Connection.php
@@ -653,7 +653,7 @@ class Xmpp_Connection
 
         $message = "<message to='" . $to . "' from='" . $this->_userName . '@'
                 . $this->_realm . '/' . $this->_resource . "' type='" . $type
-                . "' xml:lang='en'><body>" . htmlentities($text) 
+                . "' xml:lang='en'><body>" . $this->encode($text) 
                 . "</body></message>";
         $this->_stream->send($message);
 
@@ -788,6 +788,55 @@ class Xmpp_Connection
             }
         }
     }
+    
+    /**
+     * Encodes text for XML.
+     * 
+     * Most inspired by this example:
+     * http://www.sourcerally.net/Scripts/39-Convert-HTML-Entities-to-XML-Entities
+     * 
+     * @param string $text
+     * @return string 
+     */
+    private function encode($text)  
+    { 
+        $text = htmlentities($text, ENT_COMPAT, 'UTF-8');
+        $xml = array(
+            '&#34;','&#38;','&#38;','&#60;','&#62;','&#160;','&#161;','&#162;',
+            '&#163;','&#164;','&#165;','&#166;','&#167;','&#168;','&#169;','&#170;',
+            '&#171;','&#172;','&#173;','&#174;','&#175;','&#176;','&#177;','&#178;',
+            '&#179;','&#180;','&#181;','&#182;','&#183;','&#184;','&#185;','&#186;',
+            '&#187;','&#188;','&#189;','&#190;','&#191;','&#192;','&#193;','&#194;',
+            '&#195;','&#196;','&#197;','&#198;','&#199;','&#200;','&#201;','&#202;',
+            '&#203;','&#204;','&#205;','&#206;','&#207;','&#208;','&#209;','&#210;',
+            '&#211;','&#212;','&#213;','&#214;','&#215;','&#216;','&#217;','&#218;',
+            '&#219;','&#220;','&#221;','&#222;','&#223;','&#224;','&#225;','&#226;',
+            '&#227;','&#228;','&#229;','&#230;','&#231;','&#232;','&#233;','&#234;',
+            '&#235;','&#236;','&#237;','&#238;','&#239;','&#240;','&#241;','&#242;',
+            '&#243;','&#244;','&#245;','&#246;','&#247;','&#248;','&#249;','&#250;',
+            '&#251;','&#252;','&#253;','&#254;','&#255;', '&#8195;'
+        );
+        $html = array(
+            '&quot;','&amp;','&amp;','&lt;','&gt;','&nbsp;','&iexcl;','&cent;',
+            '&pound;','&curren;','&yen;','&brvbar;','&sect;','&uml;','&copy;',
+            '&ordf;','&laquo;','&not;','&shy;','&reg;','&macr;','&deg;','&plusmn;',
+            '&sup2;','&sup3;','&acute;','&micro;','&para;','&middot;','&cedil;',
+            '&sup1;','&ordm;','&raquo;','&frac14;','&frac12;','&frac34;','&iquest;',
+            '&Agrave;','&Aacute;','&Acirc;','&Atilde;','&Auml;','&Aring;','&AElig;',
+            '&Ccedil;','&Egrave;','&Eacute;','&Ecirc;','&Euml;','&Igrave;',
+            '&Iacute;','&Icirc;','&Iuml;','&ETH;','&Ntilde;','&Ograve;','&Oacute;',
+            '&Ocirc;','&Otilde;','&Ouml;','&times;','&Oslash;','&Ugrave;','&Uacute;',
+            '&Ucirc;','&Uuml;','&Yacute;','&THORN;','&szlig;','&agrave;','&aacute;',
+            '&acirc;','&atilde;','&auml;','&aring;','&aelig;','&ccedil;','&egrave;',
+            '&eacute;','&ecirc;','&euml;','&igrave;','&iacute;','&icirc;','&iuml;',
+            '&eth;','&ntilde;','&ograve;','&oacute;','&ocirc;','&otilde;','&ouml;',
+            '&divide;','&oslash;','&ugrave;','&uacute;','&ucirc;','&uuml;',
+            '&yacute;','&thorn;','&yuml;', '&emsp;'
+        );
+        $text = str_replace($html, $xml, $text); 
+        $text = str_ireplace($html, $xml, $text); 
+        return $text; 
+    } 
 
     /**
      * Checks if the server has a reserved nickname for this user in the given room.
